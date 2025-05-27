fabde7efdad0c0e421729eed9a64098ea4c41bdb
Tolga Dur <mail@tolgadur.com>
2025-05-27 17:24:54 +0100

    minor refactoring

--- src/main.py
+++ src/main.py
@@ -7,15 +7,17 @@ def scrape_london_weather():
     url = "https://weather.com/en-GB/weather/hourbyhour/l/0b8697c01baca04214b4abd206319d3eba60db5fb05c191012c4e02f6bdb23a4?unit=m"
     soup = BeautifulSoup(requests.get(url).text, "lxml")
     weather = soup.find(id=re.compile("detailIndex0$"))
+    if not weather:
+        raise ValueError("Could not find weather details.")
 
     city = "London"
-    condition = weather.find("title").text
-    temp_str = weather.find("span", {"data-testid": "TemperatureValue"}).text
+    condition = weather.find("title").get_text(strip=True).lower()
+    temp_str = weather.find("span", {"data-testid": "TemperatureValue"}).get_text(strip=True)
     temperature = int(temp_str.rstrip("°"))
 
     return {
         "city": city,
-        "condition": condition.lower(),
+        "condition": condition,
         "temperature": temperature,
     }
 
